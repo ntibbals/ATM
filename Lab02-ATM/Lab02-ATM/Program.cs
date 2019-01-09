10e6f7512c3c62760cf2bd67d19708ff244f385d
ntibbals <ntibbals@gmail.com>
2019-01-09 11:56:59 -0800

    Setup menu/switch tested display on case 1 success

--- Lab02-ATM/Lab02-ATM/Program.cs
+++ Lab02-ATM/Lab02-ATM/Program.cs
@@ -6,7 +6,37 @@ namespace Lab02_ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                MainMenu();
+            }
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Do you want to use the ATM? y/n");
+            string start = Console.ReadLine();
+            if (start == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("What transaction would you like to make?");
+                Console.WriteLine();
+                Console.WriteLine("1) View Balance ");
+                Console.WriteLine("2) Withdraw Money");
+                Console.WriteLine("3) Deposit Money ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("View Balance");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            else if (start == "n")
+                Environment.Exit(0);
         }
     }
 }
