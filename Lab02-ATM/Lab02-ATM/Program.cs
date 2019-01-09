6edbe6b9e187f5581e57cd0593a0c19ccbb5b3e4
ntibbals <ntibbals@gmail.com>
2019-01-09 11:32:52 -0800

    Setup Project and solution

---
+++ Lab02-ATM/Lab02-ATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab02_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
